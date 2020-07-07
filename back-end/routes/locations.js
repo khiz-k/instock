465cea66c0257edb1e869b3d5b7b129f4454ef79
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-07-07 07:18:19 -0700

    prelim routes for get requests

---
+++ back-end/routes/locations.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const locations = require("../../instock-data/locations.json");
+const inventory = require("../../instock-data/inventory.json");
+
+router.get("/", (req, res) => {
+  res.json(locations);
+});
+
+router.get("/:id", (req, res) => {
+  // reading error correct:
+  const { id } = req.params;
+  
+  const warehouse = locations.filter(location => location.id === id);
+  const listItem = inventory.filter(product => product.warehouseID === id);
+  res.json([warehouse, listItem]);
+});
+
+module.exports = router;
