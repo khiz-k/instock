84aa4fceafc572e19ac91029457f86677b45cac2
ryantoddgarza <ryantoddgarza@gmail.com>
2020-07-06 17:11:10 -0700

    scaffold sass. import fonts. add design system tokens, mixins, and variables based on design team requirements

--- front-end/src/components/scenes/Inventory/Inventory.jsx
+++ front-end/src/components/scenes/Inventory/Inventory.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Inventory = () => {
   return (
-    <main id="inventory" className="inventory">
+    <main id="inventory" className="container inventory">
       rendered from Inventory.jsx
     </main>
   )
--- front-end/src/components/scenes/Locations/Locations.jsx
+++ front-end/src/components/scenes/Locations/Locations.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Locations = () => {
   return (
-    <main id="locations" className="locations">
+    <main id="locations" className="container locations">
       rendered from Locations.jsx
     </main>
   )
