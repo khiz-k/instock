f70786f56c5eb5cc0732a41f39a58e7ed0954624
Aditya Sharma <kua.sharma@gmail.com>
2020-07-09 09:30:42 -0700

    event handlers implmented

--- front-end/src/components/scenes/Inventory/addNewInventory.js
+++ front-end/src/components/scenes/Inventory/addNewInventory.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "../../../styles/components/_newInventoryform.scss"
 import Modal from 'react-modal';
 
-const NewInventory = () => {
+class AddInv extends Component {
+
+    constructor(){
+        super()
+        this.state ={
+            product: '',
+            lastOrdered:'',
+            city:'',
+            country:'',
+            quantity:'',
+            isInstock: Boolean,
+            description:''
+        }
+        this.handelChange = this.handelChange.bind(this)
+
+        this.handleSubmit = (e) =>{
+            e.preventDefault();
+            // The post request for the form would go here
+
+        }
+    }
+
+    handleChange=(e)=>{
+        e.preventDefault()
+        this.setState({[e.target.name]: e.target.value });
+    }
+
+
+
+
+    render(){
     return(
         <div className="Container">
             <div className="newBox">
@@ -10,18 +40,18 @@ const NewInventory = () => {
                     <h1>Create New</h1>
                 </div>
                 <div className="newBox__form">
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div className="newBox__form--element1">
                             <label>PRODUCT</label>
-                            <input name="product" placeholder="name" type="text"></input>
+                            <input name="product" placeholder="name" type="text" onChange={this.handleChange}></input>
                             <label>LAST ORDERED</label>
-                            <input name="lastOrdered" type="date" placeholder="yyyy-mm-dd"></input>
+                            <input name="lastOrdered" type="date" placeholder="yyyy-mm-dd" onChange={this.handleChange}></input>
                         </div>
                         <div className="newBox__form--element2">
                             <label>CITY</label>
-                            <input name="city" placeholder="City" type="text"></input>
+                            <input name="city" placeholder="City" type="text" onChange={this.handleChange}></input>
                             <label>COUNTRY</label>
-                            <select name="country" placeholder="Canada" class="form-control">
+                            <select name="country" placeholder="Canada" class="form-control" onChange={this.handleChange}>
                                 <option value="Ontario">Ontario</option>
                                 <option value="Canada">Canada</option>
                                 <option value="United States of America">United States of America</option>
@@ -29,17 +59,18 @@ const NewInventory = () => {
                         </div>
                         <div className="newBox__form--element3">
                             <label>QUANTITY</label>
-                            <input name="quantity" placeholder="0" type="number"></input>
+                            <input name="quantity" placeholder="0" type="number" onChange={this.handleChange}></input>
                             <label>STATUS</label>
-                            <input></input>
+                            {/* Implment modal switch here */}
+                            <input name ="isInstock" type="checkbox" className="switch" onChange={this.handleChange}></input>
                         </div>
                         <div className="newBox__form--element3">
                             <label>ITEM DESCRIPTION</label>
-                            <textarea name="description" placeholder="(Optional)"></textarea>
+                            <textarea name="description" placeholder="(Optional)" onChange={this.handleChange}></textarea>
                         </div>
                         <div className="newBox__form--buttons">
                             <link>CANCEL</link>
-                            <button>SAVE</button>
+                            <button type="submit">SAVE</button>
                         </div>
 
                     </form>
@@ -52,4 +83,6 @@ const NewInventory = () => {
 
 }
 
-export default NewInventory;
+}
+
+export default AddInv;
