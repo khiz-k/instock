06ff82cdbf95f4593c72ab3bdde74df22c730e86
m3l1k4 <melikaihelas@gmail.com>
2020-07-11 20:08:57 -0700

    drop down menu changed from hover state to click to open and close, styling added
    this covers #10 and #11

--- front-end/src/components/scenes/Inventory/Remove.jsx
+++ front-end/src/components/scenes/Inventory/Remove.jsx
@@ -1,50 +1,52 @@
 import React, { Component } from "react";
 import kebab from "./atoms/Icon-kebab-default.svg";
 import axios from "axios";
+// import Dropdown from 'react-dropdown';
+// import 'react-dropdown/style.css';
 
 
 export default class RemoveButton extends Component {
- 
+
+  state = {
+    open: false
+  }
+
+  open = this.open.bind(this);
+  collapse = this.collapse.bind(this);
+
+  open(event) {
+    event.preventDefault();
+    this.setState({ open: true }, () => {
+      document.addEventListener('click', this.collapse);
+    });
+  }
+
+  collapse() {
+    this.setState({ open: false }, () => {
+      document.removeEventListener('click', this.collapse);
+    });
+  }
+
 
   render() {
+
     return (
-     
+
       <div className="table-row__remove">
-        <img alt = "kebab" className="table-row__remove-button" src={kebab}
-        />
-        <p>{this.props.itemID}</p>
+        <div className="table-row__remove--dropdown-handle">
+          <input onClick={this.open} className="table-row__remove-button" alt="kebab" type="image" src={kebab} />
+          <div className="table-row__remove--dropdown-content">
+            {this.state.open ? (
+              <div className="table-row__remove--dropdown-content__options" onClick={(event) => { delHandler(this.props.itemID) }} href="#"> <p>Remove</p></div>
+            ) : (null)}
+
+          </div>
+
+        </div>
       </div>
     );
   }
 }
-
-
-
-
-// export default function Remove({itemId}){
-//   return (
-//     <div className="table-row__remove">
-//       <div  className="dropdown-handle">
-//         <input className="table-row__remove-button" alt="kebab" type="image" src={kebab} />
-//         <div className="dropdown-content">
-//           <div onClick={(event) => {delHandler(itemId)}}  href="#">DELETE</div>
-//         </div>
-//         {/* <button><img alt = "kebab" className="table-row__remove-button" src={kebab}/></button> */}
-//         {/* <img alt = "kebab" className="table-row__remove-button" src={kebab}/> */}
-
-//       </div>
-//     </div>
-//   );
-
-// }
-
-
-// export default class RemoveButton extends Component {
-//   render() {
-
-//   }
-// }
-
 
 
 export function delHandler(itemId) {
@@ -52,16 +54,17 @@ export function delHandler(itemId) {
   console.log(itemId, "clicked")
 
   axios
-       .delete(`/inventory/${itemId}` )
+    .delete(`/inventory/${itemId}`)
 
-        .then(response => { 
-          console.log("item deleted")
-        })
-        .catch(error => {
-            console.log(error)
-        });
+    .then(response => {
+      console.log("item deleted")
+    })
+    .catch(error => {
+      console.log(error)
+    });
 
-       window.location.reload(false);  
-       
+  window.location.reload(false);
+
   return
 }
+
