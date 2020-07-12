71bf20caf5612497b68e930193e8a71581da72af
m3l1k4 <melikaihelas@gmail.com>
2020-07-11 18:17:32 -0700

    changed from function to passing itemid as prop

--- front-end/src/components/scenes/Inventory/Remove.jsx
+++ front-end/src/components/scenes/Inventory/Remove.jsx
@@ -2,23 +2,41 @@ import React, { Component } from "react";
 import kebab from "./atoms/Icon-kebab-default.svg";
 import axios from "axios";
 
-export default function Remove({itemId}){
-  return (
-    <div className="table-row__remove">
 
-      <div  className="dropdown-handle">
-        <input className="table-row__remove-button" alt="kebab" type="image" src={kebab} />
-        <div className="dropdown-content">
-          <div onClick={(event) => {delHandler(itemId)}}  href="#">DELETE</div>
-        </div>
-        {/* <button><img alt = "kebab" className="table-row__remove-button" src={kebab}/></button> */}
-        {/* <img alt = "kebab" className="table-row__remove-button" src={kebab}/> */}
+export default class RemoveButton extends Component {
+ 
 
+  render() {
+    return (
+     
+      <div className="table-row__remove">
+        <img alt = "kebab" className="table-row__remove-button" src={kebab}
+        />
+        <p>{this.props.itemID}</p>
       </div>
-    </div>
-  );
-
+    );
+  }
 }
+
+
+
+
+// export default function Remove({itemId}){
+//   return (
+//     <div className="table-row__remove">
+//       <div  className="dropdown-handle">
+//         <input className="table-row__remove-button" alt="kebab" type="image" src={kebab} />
+//         <div className="dropdown-content">
+//           <div onClick={(event) => {delHandler(itemId)}}  href="#">DELETE</div>
+//         </div>
+//         {/* <button><img alt = "kebab" className="table-row__remove-button" src={kebab}/></button> */}
+//         {/* <img alt = "kebab" className="table-row__remove-button" src={kebab}/> */}
+
+//       </div>
+//     </div>
+//   );
+
+// }
 
 
 // export default class RemoveButton extends Component {
