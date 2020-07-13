ef4dc2b0870fea7912bb7f210564860363df179c
ryantoddgarza <ryantoddgarza@gmail.com>
2020-07-12 20:38:03 -0700

    connect item details to back-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import Warehouses from './components/scenes/Warehouses/Warehouses';
 import Inventory from './components/scenes/Inventory/Inventory';
 import AddNewInventory from './components/scenes/Inventory/AddNewInventory/';
 import SpecificWarehouseInventory from './components/scenes/Warehouses/SpecificWarehouseInventory';
+import ItemDetails from './components/scenes/Inventory/molecules/ItemDetails.jsx';
 
 const  App = () => {
   return (
@@ -15,8 +16,8 @@ const  App = () => {
           <Route path="/" exact component={ Warehouses } />
           <Route path="/warehouses" component={ Warehouses } />
           <Route path="/warehouses/:id" component={ Warehouses } />
-          <Route path="/inventory" component={ Inventory } />
-           {/* <Route path="/inventory/:id" component={ ItemDetails } /> */}
+          <Route path="/inventory" exact component={ Inventory } />
+          <Route path="/inventory/:id" component={ ItemDetails } />
 
           <Route path="/SpecificWarehouseInventory"><SpecificWarehouseInventory/></Route> {/*placeholder, will put into the /warehouses/:id path*/}
           <Route path="/NewInventoryItem"><AddNewInventory/></Route>
