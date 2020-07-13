5ced29cc0a2f9453532f6b6d6d2aaffbe68afede
m3l1k4 <melikaihelas@gmail.com>
2020-07-12 23:09:32 -0700

    Revert "Merge pull request #27 from ryantoddgarza/OLLJONEND-12"
    This reverts commit c6cc19d6c50179467affce12bd5b9f32ddd9caba, reversing
    changes made to f1f2a65c627c585be57c2c0e4bfcb6f3e048b676.

--- front-end/src/components/scenes/Inventory/Inventory.jsx
+++ front-end/src/components/scenes/Inventory/Inventory.jsx
@@ -2,30 +2,12 @@
 import React, { Component } from "react";
 import Items from "./Items";
 import axios from "axios";
-import Switch from "react-switch";
 import SearchIcon from "./atoms/Icon-search.svg";
-import Modal from "./Modal/Modal";
-import AddInv from "./addInv";
-import { ModalProvider } from 'styled-react-modal'
-
 
 export default class Inventory extends Component {
   state = {
-    inventory: [],
-    show: false
+    inventory: []
   };
-
-  showModal=() =>{
-    this.setState({
-      ...this.state,
-      show: !this.state.show
-    })
-  }
-
-  hideModal = ()=>{
-    this.setState({show:false});
-
-  }
   componentDidMount() {
     axios.get("http://localhost:8080/inventory").then(res => {
       this.setState({
@@ -74,15 +56,8 @@ export default class Inventory extends Component {
               updateItems={this.updateItems}
               inventory={this.state.inventory}
             />
-            {/* add NewInventoryItem / modal here */}
-            <buttom className="addSign" type="button" value ="Show Modal" onClick={this.showModal}>+</buttom>
-
-            <Modal show={this.state.show}
-             onClose={this.showModal}>
-              {<AddInv/>}
-            </Modal>
-
             </div>
+            {/* AddNewInventory modal here */}
           </>
         );  
     } else {
@@ -91,4 +66,3 @@ export default class Inventory extends Component {
     }
   }
 }
-
