5ad3385755b9b1a9f11e014265a0678d43d84461
Khizar Khan <khiz_k@outlook.com>
2020-09-17 17:02:58 -0700

    fix

--- src/components/scenes/Warehouses/Warehouses.jsx
+++ src/components/scenes/Warehouses/Warehouses.jsx
@@ -7,41 +7,44 @@ import AddWarehouseModal from "./AddWarehouseModal.jsx";
 export default class Warehouses extends Component {
   state = {
     warehouses: [],
-    mobile: false
+    mobile: false,
   };
   componentDidMount() {
-    // /inventory needs to be replaced with warehouses and its respective info to fill the table rows
-    axios.get("/warehouses").then(res => {
-      this.setState({
-        warehouses: [res.data]
+    axios
+      .get("http://khiz-instock-backend.herokuapp.com/warehouses")
+      .then((res) => {
+        this.setState({
+          warehouses: [res.data],
+        });
       });
-    });
   }
   updateWarehouses = () => {
     setTimeout(() => {
-      axios.get("/warehouses").then(res => {
-      this.setState({
-        warehouses: [res.data]
-      });
-    });
+      axios
+        .get("http://khiz-instock-backend.herokuapp.com/warehouses")
+        .then((res) => {
+          this.setState({
+            warehouses: [res.data],
+          });
+        });
     }, 60);
   };
   mobileModal = () => {
     this.setState({
-      mobile: !this.state.mobile
+      mobile: !this.state.mobile,
     });
-  }
+  };
   render() {
     // if warehouses present:
     if (this.state.warehouses.length > 0) {
-        return (
-          <>
+      return (
+        <>
           <div className="container">
             <div className="inventory-header">
               <h1 className="inventory-header__title">Warehouses</h1>
               <div className="inventory-header__searchbar">
                 <img
-                  alt = "search"
+                  alt="search"
                   className="inventory-header__searchbar-icon"
                   src={SearchIcon}
                 />
@@ -51,21 +54,29 @@ export default class Warehouses extends Component {
                   placeholder="Search"
                 ></input>
               </div>
-            </div>          
+            </div>
             <div className="inventory-subHeaders">
               <span className="inventory-subHeaders__content">Warehouse</span>
               <span className="inventory-subHeaders__content">Contact</span>
-              <span className="inventory-subHeaders__content">Contact Information</span>
-              <span className="inventory-subHeaders__content categories-sub">Categories</span>
+              <span className="inventory-subHeaders__content">
+                Contact Information
+              </span>
+              <span className="inventory-subHeaders__content categories-sub">
+                Categories
+              </span>
             </div>
             <WarehousesByRow
               updateWarehouses={this.updateWarehouses}
               warehouses={this.state.warehouses}
             />
-            </div>
-            <AddWarehouseModal updateWarehouses={this.updateWarehouses} mobile={this.state.mobile} changeMobile={this.mobileModal} />
-          </>
-        );  
+          </div>
+          <AddWarehouseModal
+            updateWarehouses={this.updateWarehouses}
+            mobile={this.state.mobile}
+            changeMobile={this.mobileModal}
+          />
+        </>
+      );
     } else {
       // if no items in the inventory:
       return <h1>Loading...</h1>;
