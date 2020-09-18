3256715c17bc27ff7675ce5fe5bc77afdf3ff1d6
Khizar Khan <khiz_k@outlook.com>
2020-09-17 19:40:45 -0700

    fixing routes

--- front-end/src/components/scenes/Inventory/Remove.jsx
+++ front-end/src/components/scenes/Inventory/Remove.jsx
@@ -3,10 +3,9 @@ import kebab from "./atoms/Icon-kebab-default.svg";
 import axios from "axios";
 
 export default class RemoveButton extends Component {
-
   state = {
-    open: false
-  }
+    open: false,
+  };
 
   open = this.open.bind(this);
   collapse = this.collapse.bind(this);
@@ -14,54 +13,61 @@ export default class RemoveButton extends Component {
   open(event) {
     event.preventDefault();
     this.setState({ open: true }, () => {
-      document.addEventListener('click', this.collapse);
+      document.addEventListener("click", this.collapse);
     });
   }
 
   collapse() {
     this.setState({ open: false }, () => {
-      document.removeEventListener('click', this.collapse);
+      document.removeEventListener("click", this.collapse);
     });
   }
 
-
   render() {
-
     return (
-
       <div className="table-row__remove">
         <div className="table-row__remove--dropdown-handle">
-          <input onClick={this.open} className="table-row__remove-button" alt="kebab" type="image" src={kebab} />
+          <input
+            onClick={this.open}
+            className="table-row__remove-button"
+            alt="kebab"
+            type="image"
+            src={kebab}
+          />
           <div className="table-row__remove--dropdown-content">
             {this.state.open ? (
-              <div className="table-row__remove--dropdown-content__options" onClick={(event) => { delHandler(this.props.itemID) }} href="#"> <p>Remove</p></div>
-            ) : (null)}
-
+              <div
+                className="table-row__remove--dropdown-content__options"
+                onClick={(event) => {
+                  delHandler(this.props.itemID);
+                }}
+                href="#"
+              >
+                {" "}
+                <p>Remove</p>
+              </div>
+            ) : null}
           </div>
-
         </div>
       </div>
     );
   }
 }
 
-
 export function delHandler(itemId) {
-
-  console.log(itemId, "clicked")
+  console.log(itemId, "clicked");
 
   axios
-    .delete(`/inventory/${itemId}`)
+    .delete(`http://localhost:8080/inventory/${itemId}`)
 
-    .then(response => {
-      console.log("item deleted")
+    .then((response) => {
+      console.log("item deleted");
     })
-    .catch(error => {
-      console.log(error)
+    .catch((error) => {
+      console.log(error);
     });
 
   window.location.reload(false);
 
-  return
+  return;
 }
-
